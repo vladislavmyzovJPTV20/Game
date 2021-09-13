2d509b48bfb8d3be911166eed19b824da3ee8ba9
pupil <pupil@ivkhk.ee>
2021-09-13 13:15:29 +0300

    Добавили пять попыток с помощью цикла for

--- src/game/Game.java
+++ src/game/Game.java
@@ -18,17 +18,26 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int attempt = 0;
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         int number = random.nextInt(10);
         System.out.print("Программа задумала число от 0 до 9! Попробуй отгадать: ");
-        int userNumber = scanner.nextInt();
-        if(userNumber == number) {
-            System.out.println("Ура! Ты выиграл!");
-        }else{
-            System.out.println(":(, проиграл, задумано: "+number);
+        for(int i = 0; i < 5; i++) {
+            int userNumber = scanner.nextInt();
+            if(userNumber == number) {
+                System.out.println("Ура! Ты выиграл!");
+                break;
+            }else{
+                if(attempt < 4) {
+                    System.out.println("Неправильно, попробуй еще! ");
+            }   
+                else {
+                    System.out.println(":(, проиграл, задумано: "+number);
+                }
+              
+            }
+            attempt++;
         }
-            
     }
-    
-}
+
