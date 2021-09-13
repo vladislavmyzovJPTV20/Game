a16f852c3a29a1bc3902d1300f377aa36995a63c
pupil <pupil@ivkhk.ee>
2021-09-13 14:48:31 +0300

    Добавляем попытки в нашу игру

--- src/game/Game.java
+++ src/game/Game.java
@@ -18,30 +18,39 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int attempt = 4;
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         int number = random.nextInt(10);
         System.out.print("Программа задумала число от 0 до 9! Попробуй отгадать: ");
         //for(int i = 0; i < 5; i++) {
         int i = 0;
-        do {
+        boolean Play = true;
+        while(Play) {
             int userNumber = scanner.nextInt();
             if(userNumber == number) {
                 System.out.println("Ура! Ты выиграл!");
                 break;
             }else{
-                if(i < attempt) {
+                if(i < 4) {
                     System.out.println("Неправильно, попробуй еще! ");
             }   
                 
                 else {
                     System.out.println(":(, проиграл, задумано: "+number);
+                    if(i == 5);
+                    System.out.print("Хотите ли Вы продолжить игру: 0 - если нет, 1 - если да\n");
+                    int a = scanner.nextInt();
+                    if(userNumber == 0)
+                        break;
+                    else {
+                        Play = true;
+                    }
+                    
                 }
               
             }
             i++;
-        } while(true);
+        }
     }
 }
         
